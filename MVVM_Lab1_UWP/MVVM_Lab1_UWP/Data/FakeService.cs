126b7ce950c5e8404590e2304f489f88e8d2d905
Will Hogan <willhogan11@hotmail.com>
2016-09-20 22:16:28 +0100

    Added MVVM Directory structure and created relevant classes

---
+++ MVVM_Lab1_UWP/MVVM_Lab1_UWP/Data/FakeService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Lab1.Data
+{
+    public class Person
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class FakeService
+    {
+        public static String Name = "Fake Data Service";
+
+        public static List<Person> GetPeople()
+        {
+            Debug.WriteLine("GET for People");
+            return new List<Person>()
+            {
+                new Person() { Name="Chris Cole", Age = 10 },
+                new Person() { Name="Kelly Kale", Age=32 },
+                new Person() { Name="Dylan Durbin", Age=18 }
+            };
+        }
+
+        public static void Write(Person person)
+        {
+            Debug.WriteLine("INSERT person with name " + person.Name);
+        }
+
+        public static void Delete(Person person)
+        {
+            Debug.WriteLine("DELETE person with name " + person.Name);
+        }
+    }
+}
